57f8a6e60823112ca35bba80848f14c877d236d3
satya kumari <satyakumarigrd2004@gmail.com>
2025-10-09 22:19:20 +0530

    create a signup route

--- TractorSaathi/backend/index.js
+++ TractorSaathi/backend/index.js
@@ -1,6 +1,19 @@
 const express =require("express");
+const mongoose=require("mongoose");
 const app = express();
 const port = 3000;
+
+main().then((res)=>{
+    console.log("database connected successfully");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/tractor');
+}
+
+app.use(express.json());
+app.use("/api", require("./routes/createUser"));
 
 app.get("/",(req,res)=>{
     res.send("hello satya");
